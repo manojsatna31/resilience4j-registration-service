19222c94eb763c50659cd55beed6183c005bb7a7
wilsonselvaraj <wilsonselvaraj@gmail.com>
2021-04-25 01:32:26 +0530

    Phase 1 Commit

--- src/main/java/com/technext/spring/resilience4j/registration/management/controller/ItemRegistrationController.java
+++ src/main/java/com/technext/spring/resilience4j/registration/management/controller/ItemRegistrationController.java
@@ -2,6 +2,7 @@ package com.technext.spring.resilience4j.registration.management.controller;
 
 import com.technext.spring.resilience4j.registration.management.entity.Item;
 import com.technext.spring.resilience4j.registration.management.service.RegistrationService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,16 +10,20 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@Slf4j
+//@CrossOrigin
 public class ItemRegistrationController {
     @Autowired
     private RegistrationService registrationService;
 
     @PostMapping("/register/item")
     public String registerAsItem(@RequestBody Item item) {
+        log.info("Inside ItemRegistrationController.registerAsItem()");
         return registrationService.addItem(item);
     }
     @GetMapping("/register/itemList")
     public List<Item> getItemList() {
+        log.info("Inside ItemRegistrationController.getItemList()");
         return registrationService.getAllRegisterItems();
     }
 }
--- src/main/java/com/technext/spring/resilience4j/registration/management/controller/SellerRegistrationController.java
+++ src/main/java/com/technext/spring/resilience4j/registration/management/controller/SellerRegistrationController.java
@@ -3,6 +3,7 @@ package com.technext.spring.resilience4j.registration.management.controller;
 
 import com.technext.spring.resilience4j.registration.management.entity.Seller;
 import com.technext.spring.resilience4j.registration.management.service.RegistrationService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +11,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@Slf4j
+//@CrossOrigin
 public class SellerRegistrationController {
 
     @Autowired
@@ -17,10 +20,12 @@ public class SellerRegistrationController {
 
     @PostMapping("/register/seller")
     public String registerAsSeller(@RequestBody Seller seller) {
+        log.info("Inside ItemRegistrationController.registerAsSeller()");
         return registrationService.addSeller(seller);
     }
     @GetMapping("/register/sellerList")
     public List<Seller> getSellersList() {
+        log.info("Inside ItemRegistrationController.getItemList()");
         return registrationService.getAllRegisterSellers();
     }
 
